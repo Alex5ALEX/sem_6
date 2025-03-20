2bcaaba1f99377aeb353f58afe8de259cf599021
alex_pc <sashamelnm@gmail.com>
2025-03-20 04:36:32 +0300

    Commit_12

--- Базы_Знан/lab_1/lab_1.R
+++ Базы_Знан/lab_1/lab_1.R
@@ -1,85 +1,172 @@
-#install.packages("psych");
-#install.packages("ggplot2");
-#install.packages("lmtest");
-#install.packages("MASS");
-#install.packages("dplyr");
 
 
-print("Hello World");
+# Загрузка необходимых библиотек
+library(ggplot2)
+# Параметры нормального распределения
+mu <- 50
+sigma <- 10
+sample_size <- 120
 
-
-mu <- 50;
-sigma <- 10;
-
-
-normal_density <- function(x){
-  (1 / (sigma * sqrt(2 * pi))) * 
-    exp(-((x - mu)^2) / (2 * sigma^2));
-};
-
-x_value <- seq(20, 80, by = 0.1);
-
-
-densities <- normal_density(x_value);
-
-
-plot(x_value, densities, type = "l",
-     main="Нормальное распределение", xlab = "x",
-     ylab = "Плотность вероятности", col="blue");
-
-
-# вероятность что точка находится в зоне < 40
-probability <- pnorm(40, mean = mu, sd = sigma)
-print(probability)
-
-
-
-
-
-set.seed(1223);
-
-my_x_value <- sample(20:80, 20, replace = TRUE);
-#rm(my_x_value);
-
-my_densities <- normal_density(my_x_value);
-#rm(my_densities);
-
-plot(my_x_value, my_densities, 
-     main = "График точек", 
-     xlab = "Значения X", 
-     ylab = "Значения Y", 
-     col = "blue", 
-     pch = 19);
-
-#среднее из значений
-print(mean(my_x_value));
-
-#дисперсия из значений
-print(sigma^2);
-
-
-i<-20
-while(i <= 80)
-{
-  for(y in )
-  
-  
-  
-  
-  
-  i = i + 10;
-}
-rm(i);
-
+# Генерация данных
+set.seed(123) # Для воспроизводимости
+data <- rnorm(sample_size, mean = mu, sd = sigma)
 
 # Построение гистограммы
-hist(my_densities, breaks = 20, freq = FALSE, col = "lightblue",
-     xlab = "Значения", ylab = "Плотность", main = "Гистограмма с кривой плотности")
+hist_plot <- ggplot(data.frame(speed = data), aes(x = speed)) +
+  geom_histogram(aes(y = ..density..), bins = 30, fill = "blue",
+  color = "black", alpha = 0.7) +
+  geom_density(color = "red", size = 1) +
+  labs(title = "Гистограмма и плотность распределения скорости соединения",
+       x = "Скорость (Мбит/с)", y = "Плотность") +
+  theme_minimal()
+print(hist_plot)
 
-# Наложение кривой плотности из ваших массивов
-lines(my_x_value, my_densities, col = "red", lwd = 2)
+print(data);
 
-# Добавление легенды
-legend("topright", legend = "Плотность", col = "red", lwd = 2)
 
+
+# Вычисление вероятностей для нормального распределения
+probability_internet_speed <- dnorm(data, mean = mu, sd = sigma)
+
+print(probability_internet_speed)
+
+
+# Построение графика
+plot(data, probability_internet_speed,
+     main = "Вероятность для нормального распределения",
+     xlab = "скорость",
+     ylab = "Вероятность (probability_internet_speed)",
+     pch = 1,          # Тип точки
+     col = "black")     # Цвет точек
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Среднее значение
+mean_empirical <- mean(data)
+
+# Дисперсия
+var_empirical <- var(data)
+
+# Стандартное отклонение
+sd_empirical <- sd(data)
+
+# Вывод результатов
+cat("Эмпирическое среднее:", mean_empirical, "\n")
+
+cat("Эмпирическая дисперсия:", var_empirical, "\n")
+
+cat("Эмпирическое стандартное отклонение:", sd_empirical, "\n")
+
+
+
+
+
+
+
+
+
+# Теоретические характеристики
+mean_theoretical <- mu
+var_theoretical <- sigma^2
+sd_theoretical <- sigma
+
+# Вывод результатов
+cat("Теоретическое среднее:", mean_theoretical, "\n")
+
+cat("Теоретическая дисперсия:", var_theoretical, "\n")
+
+cat("Теоретическое стандартное отклонение:", sd_theoretical, "\n")
+
+
+
+
+
+
+
+# Разница между эмпирическими и теоретическими значениями
+diff_mean <- abs(mean_empirical - mean_theoretical)
+diff_var <- abs(var_empirical - var_theoretical)
+diff_sd <- abs(sd_empirical - sd_theoretical)
+
+# Вывод разниц
+cat("Разница в среднем значении:", diff_mean, "\n")
+
+cat("Разница в дисперсии:", diff_var, "\n")
+
+cat("Разница в стандартном отклонении:", diff_sd, "\n")
+
+
+
+
+
+
+
+
+# Вероятность P(X < 40)
+prob_less_40 <- pnorm(q = 40, mean = mu, sd = sigma)
+
+# Вывод результата
+cat("Вероятность того, что скорость соединения будет меньше 40 Мбит/с:",
+    prob_less_40, "\n")
+
+
+
+
+
+# Создание значений x для графика
+x_values <- seq(from = 20, to = 80, length.out = 1000)
+cdf_values <- pnorm(q = x_values, mean = mu, sd = sigma)
+
+# График функции распределения
+cdf_plot <- ggplot(data.frame(x = x_values, cdf = cdf_values),
+  aes(x = x, y = cdf)) +
+  geom_line(color = "blue", size = 1) +
+  labs(title = "Функция распределения скорости соединения",
+       x = "Скорость (Мбит/с)", y = "F(x)") +
+  theme_minimal()
+
+print(cdf_plot)
 
